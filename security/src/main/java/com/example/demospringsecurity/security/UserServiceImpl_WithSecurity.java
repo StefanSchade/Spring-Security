d43f1ba8e1c683625d5588230c3b020841e2156a
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2019-11-29 15:16:59 +0100

    test

---
+++ security/src/main/java/com/example/demospringsecurity/security/UserServiceImpl_WithSecurity.java
@@ -0,0 +1,58 @@
+package de.stefanschade.SpringSecurityPrototype.ws.service.impl;
+
+import de.stefanschade.SpringSecurityPrototype.domain.UserEntity;
+import de.stefanschade.SpringSecurityPrototype.repositories.UserRepository;
+import de.stefanschade.SpringSecurityPrototype.ws.dto.UserDto;
+import de.stefanschade.SpringSecurityPrototype.ws.service.UserService;
+import de.stefanschade.SpringSecurityPrototype.ws.shared.Utils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Slf4j
+@Service
+@Primary
+public class UserServiceImpl_WithSecurity implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    Utils utils;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Override
+    public UserDto createUser(UserDto user) {
+        UserEntity userEntity = new UserEntity();
+        if(userRepository.findByEmail(user.getEmail())!=null) throw new RuntimeException("Record already exists");
+        // TODO dummy values prevent exception due to non-nullable fields, they have to be generated later on
+        user.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        String publicUserId = utils.generateUserId(30);
+        user.setUserId(publicUserId);
+        BeanUtils.copyProperties(user, userEntity); // error if filed  null that is marked as non-null
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+        UserDto returnvalue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails,returnvalue);
+
+        return returnvalue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if(userEntity == null) throw new UsernameNotFoundException(email);
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
+    }
+}
